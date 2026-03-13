021013d620f54f03cf1e88d56fb73bc021edf421
SanakulovDev <sanakulovdev@gmail.com>
2026-03-13 14:29:25 +0500

    Refactor: Reformat FG Invoice form and generate absolute URLs for sales contract list.

--- _protected/views/fg-invoice/_form.php
+++ _protected/views/fg-invoice/_form.php
@@ -1,4 +1,5 @@
 <?php
+
 use app\models\Customer;
 use app\models\Factory;
 use kartik\datetime\DateTimePicker;
@@ -13,65 +14,65 @@ use yii\widgets\ActiveForm;
 /* @var TYPE_NAME $errorlist */
 ?>
 <div class="fg-invoice-form">
-  <?php $form = ActiveForm::begin(); ?>
-  <div class="row">
-    <div class="col-md-3 col-sm-3 col-lg-3">
-      <?=$form->field($model, 'invoice_no')
-              ->label(Yii::t('app', 'FG Invoice no (TTN)'))
-              ->textInput(['maxlength' => true, 'class' => 'form-control input-sm'])
-      ?>
-    </div>
-    <div class="col-md-3 col-sm-3 col-lg-3">
-      <?=$form->field($model, 'invoice_date')
-              ->label(Yii::t('app', 'Waybill date'))
-              ->widget(DateTimePicker::classname(), [
-                'type' => DateTimePicker::TYPE_COMPONENT_PREPEND,
-                'layout' => '{picker}{input}{remove}',
-                'removeButton' => ['position' => 'append'],
-                'language' => 'ru',
-                'pluginOptions' => [
-                  'autoclose' => true,
-                  'format' => 'yyyy-mm-dd',
-                  'startView' => 'month',
-                  'minView' => 'month',
-                  'maxView' => 'month',
-                ],
-                'options' => [
-                  'autocomplete' => 'off',
-                  'placeholder' => 'YYYY-MM-DD',
-                  'class' => 'form-control'
-                ]
-              ]);
-      ?>
-    </div>
-    <div class="col-md-3 col-sm-3 col-lg-3">
-      <?
-      $factories = Factory::find()->all();
-      $factory_items = ArrayHelper::map($factories, 'id', 'factoryinfo');
-      $model->factory_id = array_key_first($factory_items);
-      $params = ['prompt' => '. . .', 'class' => 'form-control'];
-      echo $form->field($model, 'factory_id')->dropDownList($factory_items, $params);
-      ?>
-    </div>
-    <div class="col-md-3 col-sm-3 col-lg-3">
-      <?
-      $customers = Customer::find()->all();
-      $customer_items = ArrayHelper::map($customers, 'id', 'name');
-      $params = ['prompt' => '. . .', 'class' => 'form-control input-sm select2'];
-      echo $form->field($model, 'customer_id')->dropDownList($customer_items, $params);
-      ?>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-md-3 col-sm-3 col-lg-3">
-      <?
-      echo $form->field($model, 'contract')->dropDownList([]);
-      ?>
-    </div>
-    <div class="col-md-2 col-sm-2 col-lg-2">
-      <?=$form->field($model, 'vat')->textInput(['value' => Yii::$app->params['vat'], 'maxlength' => true, 'class' => 'form-control'])?>
-    </div>
-    <!--
+	<?php $form = ActiveForm::begin(); ?>
+	<div class="row">
+		<div class="col-md-3 col-sm-3 col-lg-3">
+			<?= $form->field($model, 'invoice_no')
+				->label(Yii::t('app', 'FG Invoice no (TTN)'))
+				->textInput(['maxlength' => true, 'class' => 'form-control input-sm'])
+			?>
+		</div>
+		<div class="col-md-3 col-sm-3 col-lg-3">
+			<?= $form->field($model, 'invoice_date')
+				->label(Yii::t('app', 'Waybill date'))
+				->widget(DateTimePicker::classname(), [
+					'type' => DateTimePicker::TYPE_COMPONENT_PREPEND,
+					'layout' => '{picker}{input}{remove}',
+					'removeButton' => ['position' => 'append'],
+					'language' => 'ru',
+					'pluginOptions' => [
+						'autoclose' => true,
+						'format' => 'yyyy-mm-dd',
+						'startView' => 'month',
+						'minView' => 'month',
+						'maxView' => 'month',
+					],
+					'options' => [
+						'autocomplete' => 'off',
+						'placeholder' => 'YYYY-MM-DD',
+						'class' => 'form-control'
+					]
+				]);
+			?>
+		</div>
+		<div class="col-md-3 col-sm-3 col-lg-3">
+			<?
+			$factories = Factory::find()->all();
+			$factory_items = ArrayHelper::map($factories, 'id', 'factoryinfo');
+			$model->factory_id = array_key_first($factory_items);
+			$params = ['prompt' => '. . .', 'class' => 'form-control'];
+			echo $form->field($model, 'factory_id')->dropDownList($factory_items, $params);
+			?>
+		</div>
+		<div class="col-md-3 col-sm-3 col-lg-3">
+			<?
+			$customers = Customer::find()->all();
+			$customer_items = ArrayHelper::map($customers, 'id', 'name');
+			$params = ['prompt' => '. . .', 'class' => 'form-control input-sm select2'];
+			echo $form->field($model, 'customer_id')->dropDownList($customer_items, $params);
+			?>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-3 col-sm-3 col-lg-3">
+			<?
+			echo $form->field($model, 'contract')->dropDownList([]);
+			?>
+		</div>
+		<div class="col-md-2 col-sm-2 col-lg-2">
+			<?= $form->field($model, 'vat')->textInput(['value' => Yii::$app->params['vat'], 'maxlength' => true, 'class' => 'form-control']) ?>
+		</div>
+		<!--
     <div class="col-md-2 col-sm-2 col-lg-2">
 			<? /*
 				$doveronnosts = ReceivingPerson::find()->select(['fullname', 'doc_number'])->all();
@@ -94,7 +95,7 @@ use yii\widgets\ActiveForm;
 
 		<div class="col-md-2 col-sm-2 col-lg-2">
 			<?php
-    /*				$drivers = Driver::find()->select(["id", "CONCAT(first_name, ' ',last_name,' ',middle_name,'(',emp_no,')' ) as first_name"])->all();
+			/*				$drivers = Driver::find()->select(["id", "CONCAT(first_name, ' ',last_name,' ',middle_name,'(',emp_no,')' ) as first_name"])->all();
             $driver_items = ArrayHelper::map($drivers, 'first_name', 'first_name');
             $params = ['prompt' => '. . .', 'class' => 'form-control input-sm'];
           */ ?>
@@ -106,7 +107,7 @@ use yii\widgets\ActiveForm;
 
 		<div class="col-md-2 col-sm-2 col-lg-2">
 			<?php
-    /*				$trucks = Truck::find()->select(["id", "CONCAT(model, '(', number,')') as model"])->all();
+			/*				$trucks = Truck::find()->select(["id", "CONCAT(model, '(', number,')') as model"])->all();
             $truck_items = ArrayHelper::map($trucks, 'model', 'model');
             $params = ['prompt' => '. . .', 'class' => 'form-control input-sm'];
                   */ ?>
@@ -118,8 +119,8 @@ use yii\widgets\ActiveForm;
             <? /*=$form->field($model, 'truck')->textInput(['list' => "truck_list", 'maxlength' => true, 'class' => 'form-control input-sm']) */ ?>
 		</div>
     -->
-  </div>
-  <!--<div class="row">
+	</div>
+	<!--<div class="row">
 		<div class="col-md-2 col-sm-2 col-lg-2">
 			<? /*=$form->field($model, 'sender')->textInput(['maxlength' => true, 'class' => 'form-control input-sm'])*/ ?>
 		</div>
@@ -130,51 +131,52 @@ use yii\widgets\ActiveForm;
 			<? /*=$form->field($model, 'excise')->textInput(['value' => Yii::$app->params['excise'], 'maxlength' => true, 'class' => 'form-control input-sm'])*/ ?>
 		</div>
 	</div>-->
-  <div class="row">
-    <div class="col-sm-12">
-      <?=$form->field($model, 'comment')->textInput(['class' => 'form-control input-sm'])?>
-    </div>
-  </div>
-  <?
-  if(isset($errorlist) && count($errorlist)) {
-    echo '<div class="alert-danger alert fade in">';
-    echo '<strong>'.Yii::t('app', 'Error').'</strong>';
-    echo "<pre>Error:";
-    print_r($errorlist);
-    echo "</pre>";
-    echo '</div>';
-  }
-  ?>
-  <fieldset class="scheduler-border">
-    <legend class="scheduler-border">
-      <span id="addItem" class="btn btn-success btn-sm" title="<?=Yii::t('app', 'Add detail')?>">+</span>
-      <input id="selected_part_ids" type="hidden" value="0" />
-    </legend>
-    <table class="table table-striped table-bordered table-condensed table-sm-padding_2_0">
-      <thead>
-      <tr>
-        <th class="text-center"><i class="fa fa-fw fa-gears"></i></th>
-        <th><?=Yii::t('app', 'PartName')?></th>
-        <th><?=Yii::t('app', 'Qty')?></th>
-        <th><?=Yii::t('app', 'Unit')?></th>
-        <th><?=Yii::t('app', 'Price')?></th>
-        <th><?=Yii::t('app', 'Vat')?></th>
-        <!--				<th>--><? //=Yii::t('app', 'Excise')?><!--</th>-->
-      </tr>
-      </thead>
-      <tbody id="fg_inv_detail"></tbody>
-    </table>
-  </fieldset>
-  <hr>
-  <div class="form-group pull-right">
-    <?=Html::a(Yii::t('app', 'btn-cancel'), ['index'], ['class' => 'btn btn-default btn-sm'])?>
-    <?=Html::submitButton(Yii::t('app', 'btn-save'), ['class' => 'btn btn-success btn-sm'])?>
-  </div>
-  <?php ActiveForm::end(); ?>
+	<div class="row">
+		<div class="col-sm-12">
+			<?= $form->field($model, 'comment')->textInput(['class' => 'form-control input-sm']) ?>
+		</div>
+	</div>
+	<?
+	if (isset($errorlist) && count($errorlist)) {
+		echo '<div class="alert-danger alert fade in">';
+		echo '<strong>' . Yii::t('app', 'Error') . '</strong>';
+		echo "<pre>Error:";
+		print_r($errorlist);
+		echo "</pre>";
+		echo '</div>';
+	}
+	?>
+	<fieldset class="scheduler-border">
+		<legend class="scheduler-border">
+			<span id="addItem" class="btn btn-success btn-sm" title="<?= Yii::t('app', 'Add detail') ?>">+</span>
+			<input id="selected_part_ids" type="hidden" value="0" />
+		</legend>
+		<table class="table table-striped table-bordered table-condensed table-sm-padding_2_0">
+			<thead>
+				<tr>
+					<th class="text-center"><i class="fa fa-fw fa-gears"></i></th>
+					<th><?= Yii::t('app', 'PartName') ?></th>
+					<th><?= Yii::t('app', 'Qty') ?></th>
+					<th><?= Yii::t('app', 'Unit') ?></th>
+					<th><?= Yii::t('app', 'Price') ?></th>
+					<th><?= Yii::t('app', 'Vat') ?></th>
+					<!--				<th>--><? //=Yii::t('app', 'Excise')
+												?><!--</th>-->
+				</tr>
+			</thead>
+			<tbody id="fg_inv_detail"></tbody>
+		</table>
+	</fieldset>
+	<hr>
+	<div class="form-group pull-right">
+		<?= Html::a(Yii::t('app', 'btn-cancel'), ['index'], ['class' => 'btn btn-default btn-sm']) ?>
+		<?= Html::submitButton(Yii::t('app', 'btn-save'), ['class' => 'btn btn-success btn-sm']) ?>
+	</div>
+	<?php ActiveForm::end(); ?>
 </div>
 
 <?php
-$url = Url::to(['sales-contract/list-by-sales-supplier']);
+$url = Url::to(['sales-contract/list-by-sales-supplier'], true);
 $add_item = <<< JS
 $(document).ready(function() {
 	$('#fginvoice-contract').select();
